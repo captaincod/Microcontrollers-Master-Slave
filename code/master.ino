907d873ef5bafd97da42dbec1f77ce3823a16462
lesya <46486971+captaincod@users.noreply.github.com>
2021-12-27 22:41:06 +0800

    initial

---
+++ code/master.ino
@@ -0,0 +1,42 @@
+#include <SoftwareSerial.h>
+
+SoftwareSerial master_serial(10, 11);
+int ph_resistors[] = {A0, A1, A2, A3, A4};
+int array_size = sizeof(ph_resistors) / sizeof(ph_resistors[0]);
+
+void setup(){
+  
+  Serial.begin(9600);
+  
+  while (!Serial) {
+    Serial.println("not serial?");
+  }
+  
+  master_serial.begin(4800);
+}
+
+void loop(){
+  
+  if (master_serial.available()){
+    Serial.println();
+    
+    String receiving = "";
+    
+    Serial.println("Received data:");
+    while(master_serial.available()){
+      receiving += (char)master_serial.read();
+    }
+    Serial.println(receiving);
+    
+    Serial.println("This data:");
+    for (int i = 0; i < array_size; i++){
+      Serial.print("A" + (String)i + ":" + (String)analogRead(ph_resistors[i]) + "; ");
+    }
+  }
+  
+  else{
+    Serial.println("Waiting for data");
+  }
+  delay(1000);
+  Serial.println();
+}
---
+++ code/slave.ino
@@ -0,0 +1,28 @@
+#include <SoftwareSerial.h>
+
+SoftwareSerial slave_serial(10, 11);
+int ph_resistors[] = {A0, A1, A2, A3, A4};
+int array_size  = sizeof(ph_resistors) / sizeof(ph_resistors[0]);
+
+void setup(){
+  
+  Serial.begin(9600);
+  
+  while (!Serial) {
+    Serial.println("not serial?");
+  }
+  
+  slave_serial.begin(4800);
+}
+
+void loop(){
+  
+  String sending = "";
+  
+  for (int i = 0; i < array_size; i++){
+    sending += "A" + (String)i + ":" + (String)analogRead(ph_resistors[i]) + "; ";
+  }
+  
+  slave_serial.write(sending.c_str());
+  delay(1000);
+}
